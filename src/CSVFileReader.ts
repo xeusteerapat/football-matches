1813beffd2189ca2c0fac66ad232df24299ad765
xeusteerapat <xeus085@gmail.com>
2020-11-15 23:06:43 +0700

    [refactor] describe MatchData with tuple

--- src/CSVFileReader.ts
+++ src/CSVFileReader.ts
@@ -2,8 +2,10 @@ import fs from 'fs';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CSVFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
 
@@ -16,16 +18,18 @@ export class CSVFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map((row: string[]): any => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult, // type assertion
-          row[6],
-        ];
-      });
+      .map(
+        (row: string[]): MatchData => {
+          return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult, // type assertion
+            row[6],
+          ];
+        }
+      );
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { CSVFileReader } from './CSVFileReader';
 
 const reader = new CSVFileReader('football.csv');
 reader.read();
+console.log(reader.data[0]);
 
 const matches = reader.data;
 
