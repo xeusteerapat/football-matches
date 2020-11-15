3d7c5da4a0eee4dc618b5cf2bfa934328b8f4253
xeusteerapat <xeus085@gmail.com>
2020-11-15 15:41:32 +0700

    [update] read csv stats with enum

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
 import fs from 'fs';
 
-console.log(fs);
+(async function () {
+  const fsPromises = fs.promises;
+  try {
+    const matches = await (
+      await fsPromises.readFile('football.csv', {
+        encoding: 'utf-8',
+      })
+    )
+      .split('\n')
+      .map((row: string): string[] => {
+        return row.split(',');
+      });
+
+    enum MatchResult {
+      HomeWin = 'H',
+      AwayWin = 'A',
+      Draw = 'D',
+    }
+
+    let arsenalWins = 0;
+
+    for (let match of matches) {
+      if (match[1] === 'Arsenal' && match[5] === MatchResult.HomeWin) {
+        arsenalWins++;
+      } else if (match[2] === 'Arsenal' && match[5] === MatchResult.AwayWin) {
+        arsenalWins++;
+      }
+    }
+
+    console.log(`Arsenal wins ${arsenalWins} games`);
+  } catch (error) {
+    console.log(error);
+  }
+})();
