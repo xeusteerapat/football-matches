71572f7795b7fa86570443bbcffc2035a798e8c6
xeusteerapat <xeus085@gmail.com>
2020-11-18 00:22:12 +0700

    [add] abstract class for CSV

--- src/CSVFileReader.ts
+++ src/CSVFileReader.ts
@@ -1,13 +1,11 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CSVFileReader {
-  data: MatchData[] = [];
+export abstract class CSVFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
+  abstract mapRow(row: string[]): T;
+
   fsPromises = fs.promises;
 
   async read(): Promise<void> {
@@ -21,17 +19,5 @@ export class CSVFileReader {
         return row.split(',');
       })
       .map(this.mapRow);
-  }
-
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult, // type assertion
-      row[6],
-    ];
   }
 }
