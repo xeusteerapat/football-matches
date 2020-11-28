64ac065485939cc94e70d36270777d16029e1fc3
xeusteerapat <xeus085@gmail.com>
2020-11-28 15:46:52 +0700

    [edit] build and report

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
+import { Summary } from './Summary';
+import { WinAnalysis } from './analyzers/WinAnalysis';
+import { ConsoleReport } from './reportsTargets/ConsoleReport';
 import { CSVFileReader } from './CSVFileReader';
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+// create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CSVFileReader('football.csv');
 
-(async function () {
-  // create an object that satisfies the 'DataReader' interface
-  const csvFileReader = new CSVFileReader('football.csv');
+// Create an instance of MatchReader and pass in something satisfying
+// the DataReader interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-  // Create an instance of MatchReader and pass in something satisfying
-  // the DataReader interface
-  const matchReader = new MatchReader(csvFileReader);
-  matchReader.load();
+const summary = new Summary(new WinAnalysis('Arsenal'), new ConsoleReport());
 
-  const matches = matchReader.matches;
-  console.log(matches);
-})();
+summary.buildAndPrintReport(matchReader.matches);
