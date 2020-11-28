61f90be3a611a3217be44539ee559e26ce45f8f0
xeusteerapat <xeus085@gmail.com>
2020-11-28 11:30:10 +0700

    [refactor] using synchronous approach

--- src/CSVFileReader.ts
+++ src/CSVFileReader.ts
@@ -1,17 +1,37 @@
+// import fs from 'fs';
+
+// export class CSVFileReader {
+//   data: string[][] = [];
+
+//   constructor(public filename: string) {}
+//   fsPromises = fs.promises;
+
+//   async read(): Promise<void> {
+//     this.data = (
+//       await this.fsPromises.readFile(this.filename, {
+//         encoding: 'utf-8',
+//       })
+//     )
+//       .split('\n')
+//       .map((row: string): string[] => {
+//         return row.split(',');
+//       });
+//   }
+// }
+
+// Synchronous
 import fs from 'fs';
 
 export class CSVFileReader {
   data: string[][] = [];
 
   constructor(public filename: string) {}
-  fsPromises = fs.promises;
 
-  async read(): Promise<void> {
-    this.data = (
-      await this.fsPromises.readFile(this.filename, {
+  read(): void {
+    this.data = fs
+      .readFileSync(this.filename, {
         encoding: 'utf-8',
       })
-    )
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import { MatchReader } from './inheritance/MatchReader';
+import { CSVFileReader } from './CSVFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
 (async function () {
-  await reader.read();
-  console.log(reader.data[0]);
+  // create an object that satisfies the 'DataReader' interface
+  const csvFileReader = new CSVFileReader('football.csv');
 
-  const matches = reader.data;
+  // Create an instance of MatchReader and pass in something satisfying
+  // the DataReader interface
+  const matchReader = new MatchReader(csvFileReader);
+  matchReader.load();
+
+  const matches = matchReader.matches;
+  console.log(matches);
 
   let arsenalWins = 0;
 
