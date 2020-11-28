dd4ab3e736768170567366f66b0d8cbc54e8c5a4
xeusteerapat <xeus085@gmail.com>
2020-11-28 11:01:19 +0700

    [update] read method

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -9,19 +9,23 @@ interface DataReader {
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
   constructor(public reader: DataReader) {}
 
-  // .map(
-  //       (row: string[]): MatchData => {
-  //         return [
-  //           dateStringToDate(row[0]),
-  //           row[1],
-  //           row[2],
-  //           parseInt(row[3]),
-  //           parseInt(row[4]),
-  //           row[5] as MatchResult, // type assertion
-  //           row[6],
-  //         ];
-  //       }
-  //     );
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult, // type assertion
+          row[6],
+        ];
+      }
+    );
+  }
 }
